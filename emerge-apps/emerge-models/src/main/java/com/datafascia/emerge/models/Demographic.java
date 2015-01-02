91ab21c3fbe055fda644b9765d3df07bda1ee8e0
Amit Kapoor <amit@datafascia.com>
2015-01-02 10:50:53 -0800

    Add template for Emerge applications in source code
    All Emerge applications will be consolidated under one directory
    emerge-apps under which eadh individual Emerge application will reside.
    The first application is the conversion of df data to Emerge CSV format
    which is under emerge-csv. All models needed for the Emerge applications
    will be defined in emerge-models.
    
    The model in this commit is the Demographic model which maps to the
    Demographic data CSV needed by Emerge.

---
+++ emerge-apps/emerge-models/src/main/java/com/datafascia/emerge/models/Demographic.java
@@ -0,0 +1,117 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.emerge.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvGenerator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * This class is used to hold the Demographic data for Emerge and can be serialized, deserialized to
+ * and from a CSV file.
+ */
+@Slf4j @NoArgsConstructor @EqualsAndHashCode
+public class Demographic {
+  @JsonIgnore
+  private static final ObjectReader reader;
+  @JsonIgnore
+  private static final ObjectWriter writer;
+
+  static {
+    CsvMapper mapper = new CsvMapper();
+    mapper.configure(CsvGenerator.Feature.STRICT_CHECK_FOR_QUOTING, true);
+    CsvSchema.Builder builder = CsvSchema.builder();
+    for (String header : headers()) {
+      builder.addColumn(header);
+    }
+    CsvSchema schema = builder.build();
+    reader = mapper.reader(Demographic.class).with(schema);
+    writer = mapper.writer(schema);
+  }
+
+  @Getter @Setter @JsonProperty(value = "Entry #", index = 0)
+  private String entry;
+  @Getter @Setter @JsonProperty(value = "Date Created", index = 1)
+  private String  dateCreated;
+  @Getter @Setter @JsonProperty(value = "Date Updated", index = 2)
+  private String  dateUpdated;
+  @Getter @Setter @JsonProperty(value = "IP Address", index = 3)
+  private String ipAddress ;
+  @Getter @Setter @JsonProperty(value = "Data Collection Date", index = 4)
+  private String dataCollectionDate;
+  @Getter @Setter @JsonProperty(value = "JHED ID", index = 5)
+  private String jhedId ;
+  @Getter @Setter @JsonProperty(value = "Subject Patient ID", index = 6)
+  private String subjectPatientId;
+  @Getter @Setter @JsonProperty(value = "Subject Patcom", index = 7)
+  private String subjectPatcom ;
+  @Getter @Setter @JsonProperty(value = "Patient Name", index = 8)
+  private String patientName ;
+  @Getter @Setter @JsonProperty(value = "SICU Admission Date", index = 9)
+  private String sicuAdmissionDate;
+  @Getter @Setter @JsonProperty(value = "Readmission", index = 10)
+  private String readmission;
+  @Getter @Setter @JsonProperty(value = "Patient Date of Birth", index = 11)
+  private String  patientDateOfBirth;
+  @Getter @Setter @JsonProperty(value = "Gender", index = 12)
+  private String gender;
+  @Getter @Setter @JsonProperty(value = "Race", index = 13)
+  private String race;
+  @Getter @Setter @JsonProperty(value = "Patient Admission Weight (kg)", index = 14)
+  private String patientAdmissionWeightKg;
+  @Getter @Setter @JsonProperty(value = "Patient Admission Height (cm)", index = 15)
+  private String patientAdmissionHeightCm;
+  @Getter @Setter @JsonProperty(value = "Prior to Hospital Stay", index = 16)
+  private String priorToHospitalStay;
+  @Getter @Setter @JsonProperty(value = "Highest-level Activity", index = 17)
+  private String highestLevelActivity;
+  @Getter @Setter @JsonProperty(value = "Screening Tool Used", index = 18)
+  private String screeningToolUsed;
+  @Getter @Setter @JsonProperty(value = "IVC Filter", index = 19)
+  private String ivcFilter ;
+
+  /**
+   * @return the headers for the object
+   */
+  public static List<String> headers() {
+    List<String> headers = new ArrayList<String>();
+    for (Field f:Demographic.class.getDeclaredFields()) {
+      if (f.isAnnotationPresent(JsonProperty.class)) {
+        JsonProperty property = f.getAnnotation(JsonProperty.class);
+        headers.add(property.index(), property.value());
+      }
+    }
+
+    return headers;
+  }
+
+  /**
+   * Create object from string using ',' as separators
+   *
+   * @param line the line to parse values from
+   */
+  public static Demographic fromString(String line) throws IOException {
+    return reader.readValue(line);
+  }
+
+  /**
+   * Serialize the object to string using ',' as separator
+   */
+  public String asString() throws JsonProcessingException {
+    return writer.writeValueAsString(this).trim();
+  }
+}
---
+++ emerge-apps/emerge-models/src/test/java/com/datafascia/emerge/models/DemographicTest.java
@@ -0,0 +1,111 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.emerge.models;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Unit tests for Demographic model object
+ */
+@Slf4j
+public class DemographicTest {
+  String header = "Entry #,Date Created,Date Updated,IP Address,Data Collection Date,JHED ID,"
+                  + "Subject Patient ID,Subject Patcom,Patient Name,SICU Admission Date,"
+                  + "Readmission,Patient Date of Birth,Gender,Race,Patient Admission Weight (kg),"
+                  + "Patient Admission Height (cm),Prior to Hospital Stay,Highest-level Activity,"
+                  + "Screening Tool Used,IVC Filter";
+  // Test data
+  String testLine1 = "565,2014-02-18 07:37:46,2014-05-20 12:24:48,10.21.1.59,2014-02-18,dmcneli1,"
+                     + "Male170 cm109 kg,P-Male170 cm109 kg,Jud Joe Johnson,2014-05-13,No,"
+                     + "1969-01-02,Male,Black/African Am,109 kg,170 cm,NULL,NULL,Yes,No";
+  String testLine2 = "565,2014-02-18 07:37:46,2014-05-20 12:24:48,10.21.1.59,2014-02-18,dmcneli1,"
+                     + "Male170 cm109 kg,P-Male170 cm109 kg,\"Johnson, Jud\",2014-05-13,No,"
+                     + "1969-01-02,Male,Black/African Am,109 kg,170 cm,NULL,NULL,Yes,No";
+  String testLine3 = "565,2014-02-18 07:37:46,2014-05-20 12:24:48,10.21.1.59,2014-02-18,dmcneli1,"
+                     + "Male170 cm109 kg,P-Male170 cm109 kg,\"\"\"Johnson, Jud\"\"\",2014-05-13,No,"
+                     + "1969-01-02,Male,Black/African Am,109 kg,170 cm,NULL,NULL,Yes,No";
+
+  @Test
+  public void deserialisation() throws IOException {
+    assertEquals(Demographic.fromString(testLine1),getDemographic());
+  }
+
+  @Test
+  public void quoteDeserialisation() throws IOException {
+    Demographic dmg = getDemographic();
+    dmg.setPatientName("\"Johnson, Jud\"");
+    assertEquals(Demographic.fromString(testLine3),dmg);
+  }
+
+  @Test
+  public void commaDeserialisation() throws IOException {
+    Demographic dmg = getDemographic();
+    dmg.setPatientName("Johnson, Jud");
+    assertEquals(Demographic.fromString(testLine2),dmg);
+  }
+
+  @Test
+  public void header() throws JsonProcessingException {
+    String prefix = "";
+    StringBuilder sb = new StringBuilder();
+    for (String s : Demographic.headers()) {
+      sb.append(prefix);
+      sb.append(s);
+      prefix = ",";
+    }
+    assertEquals(sb.toString(), header);
+  }
+
+  @Test
+  public void quoteSerialisation() throws JsonProcessingException {
+    Demographic dmg = getDemographic();
+    dmg.setPatientName("\"Johnson, Jud\"");
+    assertEquals(dmg.asString(), testLine3);
+  }
+
+  @Test
+  public void commaSerialisation() throws JsonProcessingException {
+    Demographic dmg = getDemographic();
+    dmg.setPatientName("Johnson, Jud");
+    assertEquals(dmg.asString(), testLine2);
+  }
+
+  @Test
+  public void serialisation() throws JsonProcessingException {
+    assertEquals(getDemographic().asString(), testLine1);
+  }
+
+  /**
+   * Create test demographic object
+   */
+  private Demographic getDemographic() {
+    Demographic dmg = new Demographic();
+    dmg.setEntry("565");
+    dmg.setDateCreated("2014-02-18 07:37:46");
+    dmg.setDateUpdated("2014-05-20 12:24:48");
+    dmg.setIpAddress("10.21.1.59");
+    dmg.setDataCollectionDate("2014-02-18");
+    dmg.setJhedId("dmcneli1");
+    dmg.setSubjectPatientId("Male170 cm109 kg");
+    dmg.setSubjectPatcom("P-Male170 cm109 kg");
+    dmg.setPatientName("Jud Joe Johnson");
+    dmg.setSicuAdmissionDate("2014-05-13");
+    dmg.setReadmission("No");
+    dmg.setPatientDateOfBirth("1969-01-02");
+    dmg.setGender("Male");
+    dmg.setRace("Black/African Am");
+    dmg.setPatientAdmissionWeightKg("109 kg");
+    dmg.setPatientAdmissionHeightCm("170 cm");
+    dmg.setPriorToHospitalStay("NULL");
+    dmg.setHighestLevelActivity("NULL");
+    dmg.setScreeningToolUsed("Yes");
+    dmg.setIvcFilter("No");
+
+    return dmg;
+  }
+}

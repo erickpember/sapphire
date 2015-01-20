3a7ef0ea37e950b3fb7c2318c03e7ba5b9545d6f
Amit Kapoor <amit@datafascia.com>
2015-01-20 08:17:01 -0800

    Add missing files for previous commit
    SimpleAuthenticator and User.java should have been part of the previous
    commit a25a1d1b2b54da738e3c1702011

---
+++ api-server/src/main/java/com/datafascia/api/authenticator/SimpleAuthenticator.java
@@ -0,0 +1,23 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.api.authenticator;
+
+import com.google.common.base.Optional;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+/**
+ * Simple authenticator for API access
+ */
+public class SimpleAuthenticator implements Authenticator<BasicCredentials, User> {
+  @Override
+  public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+    if ("6n'48i?cNHT388b".equals(credentials.getPassword())) {
+      return Optional.of(new User(credentials.getUsername()));
+    }
+
+    return Optional.absent();
+  }
+}
+

7b1fd9102928af9fec4ba4bd59d33209afaf4bb3
Amit Kapoor <amit@datafascia.com>
2015-01-31 12:24:32 -0800

    Add function to return classes with a particular type annotation
    This will be useful to scan packages to get classes only with that
    annotation at type level.

--- common/src/main/java/com/datafascia/reflections/PackageUtils.java
+++ common/src/main/java/com/datafascia/reflections/PackageUtils.java
@@ -2,11 +2,13 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.reflections;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
@@ -28,5 +30,21 @@ public class PackageUtils {
         .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageName))));
 
     return reflections.getSubTypesOf(Object.class);
+  }
+
+  /**
+   * @param packageName the package name to get all classes of
+   * @param annotation the annotation class
+   *
+   * @return list of classes in package having the particular type annotation
+   */
+  public static Set<Class<?>> withTypeAnnotations(String packageName,
+      Class<? extends Annotation> annotation) {
+    Reflections reflections = new Reflections(new ConfigurationBuilder()
+        .setScanners(new SubTypesScanner(false), new TypeAnnotationsScanner())
+        .setUrls(ClasspathHelper.forPackage(packageName))
+        .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageName))));
+
+    return reflections.getTypesAnnotatedWith(annotation);
   }
 }

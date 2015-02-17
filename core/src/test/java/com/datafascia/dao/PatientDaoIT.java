8606f54f7eaa19ac2ff71994ccee7fd11d979daf
Amit Kapoor <amit@datafascia.com>
2015-02-16 17:10:24 -0800

    Adds teeth to the patient data accessor test
    Just testing the total number of patients returned is not sufficient.
    The test now checks that patient values being returned are correct.

--- core/src/test/java/com/datafascia/dao/PatientDaoIT.java
+++ core/src/test/java/com/datafascia/dao/PatientDaoIT.java
@@ -3,7 +3,10 @@
 package com.datafascia.dao;
 
 import com.datafascia.models.Patient;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.TimeZone;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
@@ -14,8 +17,16 @@ import static org.testng.Assert.assertEquals;
  */
 @Slf4j
 public class PatientDaoIT extends DaoIT {
+  /**
+   * Test the presence and validity of patients.
+   *
+   * @throws Exception should not happen
+   */
   @Test
   public void patients() throws Exception {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
     log.info("Getting the patients");
     PatientDao patientDao = new PatientDao(queryTemplate);
     Iterator<Patient> patients = patientDao.patients(true);
@@ -23,9 +34,63 @@ public class PatientDaoIT extends DaoIT {
     int count = 0;
     while (patients.hasNext()) {
       Patient patient = patients.next();
+      switch (patient.getId().toString()) {
+        case "urn:df-patientId-1:96087004":
+          validatePatient(patient, "ECMNOTES", null, "TEST",
+              dateFormat.parse("1977-01-01T05:00:00Z"), "urn:df-patientId-1:96087004",
+              "urn:df-institution-patientId-1:UCSF::96087004");
+          break;
+        case "urn:df-patientId-1:96087039":
+          validatePatient(patient, "ONE", "A", "ECM-MSSGE",
+              dateFormat.parse("1960-06-06T04:00:00Z"), "urn:df-patientId-1:96087039",
+              "urn:df-institution-patientId-1:UCSF::96087039");
+          break;
+        case "urn:df-patientId-1:96087047":
+          validatePatient(patient, "ONE", "B", "ECM-MSSGE",
+              dateFormat.parse("1954-10-29T05:00:00Z"), "urn:df-patientId-1:96087047",
+              "urn:df-institution-patientId-1:UCSF:SICU:96087047");
+          break;
+        case "urn:df-patientId-1:96087055":
+          validatePatient(patient, "ONE", "C", "ECM-MSSGE",
+              dateFormat.parse("1996-07-29T04:00:00Z"), "urn:df-patientId-1:96087055",
+              "urn:df-institution-patientId-1:UCSF::96087055");
+          break;
+        case "urn:df-patientId-1:96087063":
+          validatePatient(patient, "ONE", "D", "ECM-MSSGE",
+              dateFormat.parse("1977-10-29T04:00:00Z"), "urn:df-patientId-1:96087063",
+              "urn:df-institution-patientId-1:UCSF::96087063");
+          break;
+        case "urn:df-patientId-1:97534012":
+          validatePatient(patient, "ONEFIVE", "C", "MB-CHILD",
+              dateFormat.parse("1999-02-20T05:00:00Z"), "urn:df-patientId-1:97534012",
+              "urn:df-institution-patientId-1:UCSF:SICU:97534012");
+          break;
+      }
+
       count++;
     }
 
     assertEquals(count, 703);
+  }
+
+  /**
+   * Validates a patient object against various expected values.
+   *
+   * @param patient the patient object
+   * @param firstName first name
+   * @param middleName middle name
+   * @param lastName last name
+   * @param birthDate date of birth
+   * @param patId patient identifier
+   * @param instId institution identifier
+   */
+  public void validatePatient(Patient patient, String firstName, String middleName, String lastName,
+      Date birthDate, String patId, String instId) {
+    assertEquals(patient.getName().getFirst(), firstName);
+    assertEquals(patient.getName().getMiddle(), middleName);
+    assertEquals(patient.getName().getLast(), lastName);
+    assertEquals(patient.getBirthDate(), birthDate);
+    assertEquals(patient.getId().toString(), patId);
+    assertEquals(patient.getInstitutionPatientId().toString(), instId);
   }
 }

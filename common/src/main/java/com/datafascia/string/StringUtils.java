bc70dd530b0b681fbaed994269a767d5ba3df2d1
Chin Huang <chinhuang@datafascia.com>
2015-02-19 16:41:20 -0800

    Simplify string conversion to UTF-8
    Remove catch of UnsupportedEncodingException because Java must implement
    UTF-8.

--- common/src/main/java/com/datafascia/string/StringUtils.java
+++ common/src/main/java/com/datafascia/string/StringUtils.java
@@ -2,30 +2,25 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.string;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Utility string functions
  */
-@Slf4j
 public class StringUtils {
 
   /**
-   * @param string the string
+   * Base 64 encodes UTF-8 formatted string
    *
-   * @return the string in Base64 form
+   * @param string the string
+   * @return the string in base 64 form
    */
   public static String base64Encode(String string) {
-    try {
-      return Base64.getEncoder().encodeToString(string.getBytes("utf-8"));
-    } catch (UnsupportedEncodingException ex) {
-      log.error("UTF-8 needs to be supported", ex);
-      throw new RuntimeException(ex);
-    }
+    return Base64.getEncoder().encodeToString(string.getBytes(StandardCharsets.UTF_8));
   }
 
+  // Private constructor disallows creating instances of this class
   private StringUtils() {
   }
 }

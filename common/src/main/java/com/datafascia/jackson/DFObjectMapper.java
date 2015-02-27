3dfe949ccd91ec0fe9000f021e19fec6146c34b8
Jim Smyth <jimsmyth@datafascia.com>
2015-02-27 13:17:24 -0500

    Creates DFObjectMapper to hold singleton of ObjectMapper
    The ObjectMapper will load various dF specific features to the
    underlying object mapper so that clients do not have to be aware
    of them.

---
+++ common/src/main/java/com/datafascia/jackson/DFObjectMapper.java
@@ -0,0 +1,23 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Utility class to create dF instance of object mapper. This ensures that all necessary
+ * serializer/deserializers are loaded
+ */
+@Slf4j
+public class DFObjectMapper {
+  /**
+   * @return dF instance of Jackson ObjectMapper with all modules loaded
+   */
+  public static ObjectMapper objectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new UnitsMapperModule());
+
+    return mapper;
+  }
+}

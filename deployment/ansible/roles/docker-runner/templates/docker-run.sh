b3425412a5b7f57a4fa9d138fdff8dfa026bd236
Chin Huang <chinhuang@datafascia.com>
2015-03-16 14:41:41 -0700

    Run ZooKeeper in Docker container in local Vagrant virtual machine

---
+++ deployment/ansible/roles/docker-runner/templates/docker-run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -lt 2 ]; then
+  echo "usage: $0 host_name run_arguments"
+  exit 1
+fi
+hostName=$1
+shift 1
+set -ex
+
+containerId=$(docker run --name=$hostName {{ docker_run_options }} $*)
+
+ipAddress=$(docker inspect --format {{ "'{{ .NetworkSettings.IPAddress }}'" }} $containerId)
+
+echo "host-record=$hostName,$ipAddress" >{{ dnsmasq_conf_dir }}/$hostName
+service dnsmasq restart
+
+# Delay because systemd allows at most 5 start requests in 10 seconds.
+sleep 2
+
+echo $containerId

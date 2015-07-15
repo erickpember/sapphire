a32cc75c4fb38c15fba2ee8f13910219c77b0032
Chin Huang <chinhuang@datafascia.com>
2015-07-15 00:28:04 -0700

    Fix hanging unit test
    Before this change, AccumuloImportTest started a mini Accumulo cluster,
    and was hanging in Jenkins. Change the test to use a mock Accumulo
    instance.

--- common/core/src/test/java/com/datafascia/common/accumulo/AccumuloImportTest.java
+++ common/core/src/test/java/com/datafascia/common/accumulo/AccumuloImportTest.java
@@ -2,9 +2,6 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.common.accumulo;
 
-import com.datafascia.common.accumulo.AccumuloConfiguration;
-import com.datafascia.common.accumulo.AccumuloImport;
-import com.datafascia.common.accumulo.AccumuloModule;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import com.google.inject.AbstractModule;
@@ -13,66 +10,43 @@ import com.google.inject.Injector;
 import com.google.inject.Provides;
 import java.io.File;
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.accumulo.core.client.Connector;
-import org.apache.accumulo.minicluster.MiniAccumuloCluster;
-import org.apache.accumulo.minicluster.MiniAccumuloConfig;
-import org.apache.commons.io.FileUtils;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
  * Import data to Accumulo test
  */
-@Slf4j
 public class AccumuloImportTest {
-  private static final String USER = "root";
-  private static final String PASSWORD = "secret";
 
-  private File tempDir;
-  private MiniAccumuloCluster cluster;
-  private Injector injector;
+  private Connector connector;
 
   @BeforeClass
   public void setup() throws IOException, InterruptedException {
-    tempDir = Files.createTempDir();
-    cluster = new MiniAccumuloCluster(
-        new MiniAccumuloConfig(tempDir, PASSWORD));
-    cluster.start();
-
-    injector = Guice.createInjector(
+    Injector injector = Guice.createInjector(
         new AbstractModule() {
           @Override
           protected void configure() {
           }
 
           @Provides
-          public AccumuloConfiguration getAccumuloConfiguration() {
+          public AccumuloConfiguration accumuloConfiguration() {
             return AccumuloConfiguration.builder()
-                .instance(cluster.getInstanceName())
-                .zooKeepers(cluster.getZooKeepers())
-                .user(USER)
-                .password(PASSWORD)
+                .instance(ConnectorFactory.MOCK_INSTANCE)
+                .zooKeepers("")
+                .user("root")
+                .password("")
                 .build();
           }
         },
         new AccumuloModule());
-  }
 
-  @AfterClass
-  public void teardown() throws Exception {
-    cluster.stop();
-    FileUtils.deleteDirectory(tempDir);
+    connector = injector.getInstance(Connector.class);
   }
 
   @Test
   public void importData() throws Exception {
-    // Give a little time for mini-cluster to start
-    Thread.sleep(9000);
-
-    Connector connector = injector.getInstance(Connector.class);
-    String resourceFile = Resources.getResource("sample.rf").getPath();
+    String resourceFile = Resources.getResource("accumulo-data/sample.rf").getPath();
     String path = resourceFile.substring(0, resourceFile.lastIndexOf(File.separator));
     AccumuloImport.importData(connector, "test", path, Files.createTempDir().getPath());
   }

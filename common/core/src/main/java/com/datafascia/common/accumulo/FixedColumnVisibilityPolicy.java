6c32c3053f406e5f87d8e71fd09b57397b25b881
Chin Huang <chinhuang@datafascia.com>
2015-08-21 13:07:38 -0700

    Supply empty Accumulo column visibility expression

--- common/core/src/main/java/com/datafascia/common/accumulo/FixedColumnVisibilityPolicy.java
+++ common/core/src/main/java/com/datafascia/common/accumulo/FixedColumnVisibilityPolicy.java
@@ -2,19 +2,34 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.common.accumulo;
 
-import lombok.NoArgsConstructor;
 import org.apache.accumulo.core.security.ColumnVisibility;
 
 /**
  * Supplies the same visibility expression throughout the lifetime of the application.
  */
-@NoArgsConstructor
 public class FixedColumnVisibilityPolicy implements ColumnVisibilityPolicy {
 
-  private static final ColumnVisibility COLUMN_VISIBILITY = new ColumnVisibility("System");
+  private final ColumnVisibility columnVisibility;
+
+  /**
+   * Constructs policy that supplies given visibility expression.
+   *
+   * @param columnVisibility
+   *     visibility expression
+   */
+  public FixedColumnVisibilityPolicy(ColumnVisibility columnVisibility) {
+    this.columnVisibility = columnVisibility;
+  }
+
+  /**
+   * Constructs policy that supplies empty visibility expression.
+   */
+  public FixedColumnVisibilityPolicy() {
+    this(new ColumnVisibility());
+  }
 
   @Override
   public ColumnVisibility getColumnVisibility(String tableName, String columnQualifier) {
-    return COLUMN_VISIBILITY;
+    return columnVisibility;
   }
 }

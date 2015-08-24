346671c53062693b76d6c0bfaf24e7455ac6935b
Jim Smyth <jimsmyth@datafascia.com>
2015-08-24 15:03:56 -0400

    Add nursing order NiFi processor

---
+++ etl/web-services-ingest/src/main/java/com/datafascia/etl/ucsf/web/NursingOrdersProcessor.java
@@ -0,0 +1,243 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.etl.ucsf.web;
+
+import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
+import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
+import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
+import ca.uhn.fhir.model.dstu2.composite.TimingDt;
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import ca.uhn.fhir.model.dstu2.resource.ProcedureRequest;
+import ca.uhn.fhir.model.dstu2.valueset.ProcedureRequestStatusEnum;
+import ca.uhn.fhir.model.primitive.DateTimeDt;
+import ca.uhn.fhir.model.primitive.StringDt;
+import com.datafascia.api.client.ClientBuilder;
+import com.datafascia.domain.fhir.CodingSystems;
+import com.datafascia.domain.fhir.IdentifierSystems;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ProcessorLog;
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.ProcessorInitializationContext;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * Processor for handling nursing orders at UCSF.
+ */
+@Slf4j
+public class NursingOrdersProcessor extends AbstractProcessor {
+  private Set<Relationship> relationships;
+  private List<PropertyDescriptor> properties;
+  private ClientBuilder clientBuilder = null;
+
+  public static final Relationship SUCCESS = new Relationship.Builder()
+      .name("SUCCESS")
+      .description("Success relationship")
+      .build();
+  public static final Relationship FAILURE = new Relationship.Builder()
+      .name("FAILURE")
+      .description("Failure relationship")
+      .build();
+  public static final PropertyDescriptor FHIR_SERVER = new PropertyDescriptor.Builder()
+      .name("Fhir server connection.")
+      .required(true)
+      .addValidator(StandardValidators.URL_VALIDATOR)
+      .build();
+  public static final PropertyDescriptor FHIR_USERNAME = new PropertyDescriptor.Builder()
+      .name("Fhir server username.")
+      .required(false)
+      .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+      .build();
+  public static final PropertyDescriptor FHIR_PASSWORD = new PropertyDescriptor.Builder()
+      .name("Fhir server password.")
+      .required(false)
+      .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+      .build();
+
+  @Override
+  public Set<Relationship> getRelationships() {
+    return relationships;
+  }
+
+  @Override
+  public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+    return properties;
+  }
+
+  @Override
+  public void init(final ProcessorInitializationContext context) {
+    List<PropertyDescriptor> initProperties = new ArrayList<>();
+    initProperties.add(FHIR_SERVER);
+    initProperties.add(FHIR_USERNAME);
+    initProperties.add(FHIR_PASSWORD);
+    this.properties = Collections.unmodifiableList(initProperties);
+
+    Set<Relationship> initRelationships = new HashSet<>();
+    initRelationships.add(SUCCESS);
+    initRelationships.add(FAILURE);
+    this.relationships = Collections.unmodifiableSet(initRelationships);
+  }
+
+  @Override
+  public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+    if (clientBuilder == null) {
+      clientBuilder = new ClientBuilder(context.getProperty(FHIR_SERVER).getValue(),
+          context.getProperty(FHIR_USERNAME).getValue(),
+          context.getProperty(FHIR_PASSWORD).getValue());
+    }
+
+    final ProcessorLog plog = this.getLogger();
+    final FlowFile flowfile = session.get();
+
+    session.read(flowfile, new InputStreamCallback() {
+
+      @Override
+      public void process(InputStream in) throws IOException {
+        try {
+          String jsonString = IOUtils.toString(in);
+          JSONObject jsonObject = (JSONObject) new JSONParser().parse(jsonString);
+
+          Object errorObj = jsonObject.get("Error");
+          if (errorObj != null) {
+            log.error("Nursing order web service error: " + errorObj.toString());
+          }
+
+          JSONArray patients = (JSONArray) jsonObject.get("Patient");
+          for (Object obj : patients) {
+            if (obj instanceof JSONObject) {
+              String encounterId = ((JSONObject) obj).get("CSN").toString();
+              JSONArray orders = (JSONArray) ((JSONObject) obj).get("Orders");
+              for (Object obj2 : orders) {
+                if (obj2 instanceof JSONObject) {
+                  JSONObject jsonNursing = (JSONObject) obj2;
+                  String status = jsonNursing.get("OrderStatus").toString();
+                  ProcedureRequest procedure = populateProcedureRequest(jsonNursing, encounterId);
+                  if (status.equals("2^Sent")) {
+                    clientBuilder.getProcedureRequestClient().saveProcedureRequest(procedure,
+                        encounterId);
+                  } else if (status.equals("4^Canceled")) {
+                    clientBuilder.getProcedureRequestClient().updateProcedureRequest(procedure,
+                        encounterId);
+                  }
+                }
+              }
+            }
+          }
+        } catch (ParseException e) {
+          log.error("Error reading json.", e);
+          plog.error("Failed to read json string: " + e.getMessage());
+          throw new ProcessException(e);
+        }
+      }
+    });
+    session.transfer(flowfile, SUCCESS);
+  }
+
+  private ProcedureRequest populateProcedureRequest(JSONObject jsonNursing, String encounterId) {
+    String status = jsonNursing.get("OrderStatus").toString();
+    String orderId = jsonNursing.get("OrderID").toString();
+    String procID = jsonNursing.get("ProcID").toString();
+    String orderDesc = jsonNursing.get("OrderDesc").toString();
+    String startDate = jsonNursing.get("StartDate").toString();
+    String discontinuedDate = jsonNursing.get("DiscontinuedDate").toString();
+
+    // Determine if the order has been signed.
+    boolean signed = false;
+    Object auditObject = jsonNursing.get("Audit");
+    if (auditObject != null) {
+      for (Object obj : (JSONArray) auditObject) {
+        String action = ((JSONObject) obj).get("Action").toString();
+        if (action.equals("3^Signed")) {
+          signed = true;
+          break;
+        }
+      }
+    }
+
+    ProcedureRequest procedure = new ProcedureRequest();
+    procedure.addIdentifier()
+        .setSystem(IdentifierSystems.INSTITUTION_PROCEDURE_REQUEST)
+        .setValue(orderId);
+
+    if (signed) {
+      procedure.setStatus(ProcedureRequestStatusEnum.ACCEPTED);
+    } else if (status.equals("2^Sent")) {
+      procedure.setStatus(ProcedureRequestStatusEnum.PROPOSED);
+    }
+
+    if (status.equals("4^Canceled")) {
+      procedure.setStatus(ProcedureRequestStatusEnum.COMPLETED);
+    }
+
+    TimingDt timing = new TimingDt();
+    PeriodDt period = new PeriodDt();
+    /* Null or otherwise 0 dates (they manifest differently from system to system) may throw
+     * exceptions which can be safely ignored. We only care about real data. */
+    try {
+      Instant startInstant = UcsfWebGetProcessor.epicDateToInstant(startDate);
+      period.setStart(new DateTimeDt(Date.from(startInstant)));
+    } catch (DateTimeException e) {
+    }
+
+    // The string here is a null date, which we don't want to use.
+    if (!discontinuedDate.equals("/Date(-62135568000000-0800)/")) {
+      try {
+        Instant endInstant = UcsfWebGetProcessor.epicDateToInstant(discontinuedDate);
+        period.setEnd(new DateTimeDt(Date.from(endInstant)));
+      } catch (DateTimeException e) {
+      }
+    }
+    TimingDt.Repeat repeat = new TimingDt.Repeat();
+    repeat.setBounds(period);
+    timing.setRepeat(repeat);
+    procedure.setTiming(timing);
+    procedure.getTiming();
+
+    CodeableConceptDt concept = new CodeableConceptDt(CodingSystems.PROCEDURE_REQUEST_TYPE,
+        procID);
+    concept.setText(orderDesc);
+    procedure.setType(concept);
+
+    Encounter encounter = clientBuilder.getEncounterClient().getEncounter(encounterId);
+    if (encounter != null) {
+      procedure.setEncounter(new ResourceReferenceDt(encounter.getId()));
+    } else {
+      log.warn("Could not find encounter with CSN " + encounterId + ". HAPI FHIR doesn't allow "
+          + "dangling references, so the linkage between the encounter and nursing order " + orderId
+          + " is lost.");
+    }
+
+    ArrayList<StringDt> notes = new ArrayList<>();
+    Object questions = jsonNursing.get("Questions");
+    if (questions != null) {
+      for (Object obj : ((JSONArray) questions)) {
+        String answer = ((JSONObject) obj).get("Answer").toString();
+        notes.add(new StringDt(answer));
+      }
+    }
+    procedure.setNotes(notes);
+
+    return procedure;
+  }
+}
---
+++ integration-tests/src/test/java/com/datafascia/etl/NursingOrderProcessorIT.java
@@ -0,0 +1,306 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.etl;
+
+import ca.uhn.fhir.model.dstu2.composite.TimingDt;
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import ca.uhn.fhir.model.dstu2.resource.ProcedureRequest;
+import ca.uhn.fhir.model.dstu2.valueset.EncounterStateEnum;
+import ca.uhn.fhir.model.dstu2.valueset.ProcedureRequestStatusEnum;
+import ca.uhn.fhir.model.primitive.IdDt;
+import ca.uhn.fhir.model.primitive.StringDt;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import com.datafascia.api.client.ClientBuilder;
+import com.datafascia.api.services.ApiIT;
+import com.datafascia.domain.fhir.IdentifierSystems;
+import com.datafascia.etl.ucsf.web.NursingOrdersProcessor;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Integration tests for the NursingOrderProcessor.
+ */
+@Slf4j
+public class NursingOrderProcessorIT extends ApiIT {
+  private NursingOrdersProcessor processor;
+  private ClientBuilder clientBuilder;
+  private Encounter transientEncounter;
+
+  @BeforeClass
+  public void beforeRepositoryTestSupport() throws Exception {
+    Injector injector = Guice.createInjector(new TestModule());
+    injector.injectMembers(this);
+  }
+
+  @BeforeClass
+  public void setup() throws Exception {
+    transientEncounter = createAnEncounterWithApi();
+  }
+
+  @AfterClass
+  public void close() throws Exception {
+    client.delete().resource(transientEncounter).execute();
+  }
+
+  private Encounter createAnEncounterWithApi() {
+    Encounter encounter1 = new Encounter();
+    encounter1.addIdentifier()
+        .setSystem(IdentifierSystems.INSTITUTION_ENCOUNTER).setValue("2085202");
+    encounter1.setStatus(EncounterStateEnum.IN_PROGRESS);
+    MethodOutcome outcome = client.create().resource(encounter1)
+        .encodedJson().execute();
+    IdDt id = outcome.getId();
+    encounter1.setId(id);
+    return encounter1;
+  }
+
+  int enqueueCount = 0;
+
+  @Test
+  public void testOnTrigger() throws Exception {
+    Thread.sleep(2000);
+
+    TestRunner runner = TestRunners.newTestRunner(NursingOrdersProcessor.class);
+    runner.setProperty(NursingOrdersProcessor.FHIR_SERVER, fhirServer);
+    runner.setProperty(NursingOrdersProcessor.FHIR_USERNAME, FHIR_USERNAME);
+    runner.setProperty(NursingOrdersProcessor.FHIR_PASSWORD, FHIR_PASSWORD);
+
+    clientBuilder = new ClientBuilder(fhirServer, FHIR_USERNAME, FHIR_PASSWORD);
+
+    processor = (NursingOrdersProcessor) runner.getProcessor();
+
+    // Positive tests of new data and diffs.
+    runner.enqueue(addContent("GetOrdersByUnit.json"));
+    runner.enqueue(addContent("GetOrdersByUnit-2.json"));
+    runner.enqueue(addContent("GetOrdersByUnit-3.json"));
+    runner.enqueue(addContent("GetOrdersByUnit-4.json"));
+    runner.enqueue(addContent("GetOrdersByUnit-5.json"));
+    runner.enqueue(addContent("GetOrdersByUnit-6.json"));
+    runner.run(enqueueCount);
+    runner.assertQueueEmpty();
+    runner.assertAllFlowFilesTransferred(NursingOrdersProcessor.SUCCESS);
+
+    for (String key : expectedOrders.keySet()) {
+      ExpectedOrder expectedOrder = expectedOrders.get(key);
+
+      ProcedureRequest storedRequest = clientBuilder.getProcedureRequestClient()
+          .getProcedureRequest(key, "2085202");
+
+      assertNotNull(storedRequest);
+      assertEquals(storedRequest.getEncounter().getReference().getIdPart(), expectedOrder
+          .getEncounter());
+      assertEquals(storedRequest.getType().getCodingFirstRep().getCode(), expectedOrder
+          .getProcedureId());
+      assertEquals(storedRequest.getType().getText(), expectedOrder
+          .getDescription());
+      assertEquals(storedRequest.getIdentifierFirstRep().getValue(), expectedOrder.getOrderId());
+      assertEquals(((TimingDt)storedRequest.getTiming()).getRepeat().getBounds().getStart(),
+          expectedOrder.getStartDate());
+      assertEquals(((TimingDt)storedRequest.getTiming()).getRepeat().getBounds().getEnd(),
+          expectedOrder.getDiscontinuedDate());
+
+      for (StringDt note : storedRequest.getNotes()) {
+        assertTrue(expectedOrder.getQuestions().contains(note.toString()));
+      }
+    }
+  }
+
+  private InputStream addContent(String filename) throws FileNotFoundException {
+    enqueueCount++;
+    return new FileInputStream("src/test/resources/nursingOrderProcessor/" + filename);
+  }
+
+  private HashMap<String, ExpectedOrder> expectedOrders = new HashMap<String, ExpectedOrder>() {
+    {
+      put("1689469", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("87881");
+          setDescription("No VTE Prevention Indicated");
+          setOrderId("1689469");
+          setStartDate(new Date(1439398800000l));
+          setDiscontinuedDate(new Date(1439527220000l));
+          setStatus(ProcedureRequestStatusEnum.COMPLETED);
+          setQuestions(new ArrayList<String>() {
+            {
+              add("No VTEP because patient at very low risk for VTE (VTE score < 4 and walking " +
+                  "unassisted or will be discharged within 24 hours)");
+            }
+          });
+        }
+      });
+      put("1689484", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("7936");
+          setDescription("Maintain sequential compression device");
+          setOrderId("1689484");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.PROPOSED);
+          setQuestions(new ArrayList<>());
+        }
+      });
+      put("1689485", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("7820");
+          setDescription("Place sequential compression device");
+          setOrderId("1689485");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.PROPOSED);
+          setQuestions(new ArrayList<>());
+        }
+      });
+      put("1689486", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("40999");
+          setDescription("Remove sequential compression device");
+          setOrderId("1689486");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.PROPOSED);
+          setQuestions(new ArrayList<>());
+        }
+      });
+      put("1689487", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("40739");
+          setDescription("Head of bed flat");
+          setOrderId("1689487");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.PROPOSED);
+          setQuestions(new ArrayList<>());
+        }
+      });
+      put("1689488", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("57652");
+          setDescription("Keep prone");
+          setOrderId("1689488");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.PROPOSED);
+          setQuestions(new ArrayList<>());
+        }
+      });
+      put("1689490", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("124838");
+          setDescription("RASS = Target RASS -3: Moderate Sedation (acceptable range -4 to -2)");
+          setOrderId("1689490");
+          setStartDate(new Date(1439485200000l));
+          setDiscontinuedDate(new Date(1439527295000l));
+          setStatus(ProcedureRequestStatusEnum.COMPLETED);
+          setQuestions(new ArrayList<String>() {
+            {
+              add("Target RASS -3: Moderate Sedation (acceptable range -4 to -2)");
+            }
+          });
+        }
+      });
+      put("1689478", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("87881");
+          setDescription("No VTE Prevention Indicated");
+          setOrderId("1689478");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.ACCEPTED);
+          setQuestions(new ArrayList<String>() {
+            {
+              add("No additional mechanical/pharmacologic VTEP because patient is already " +
+                  "therapeutically anticoagulated (e.g., warfarin, heparin, etc.)");
+            }
+          });
+        }
+      });
+      put("1689477", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("143391");
+          setDescription("Bed Rest with HOB <= 30 Degrees Continuous");
+          setOrderId("1689477");
+          setStartDate(new Date(1439485200000l));
+          setDiscontinuedDate(new Date(1439527424000l));
+          setStatus(ProcedureRequestStatusEnum.COMPLETED);
+          setQuestions(new ArrayList<String>() {
+            {
+              add("Bathroom Privileges");
+              add("May stand to void");
+              add("Other (specify)");
+            }
+          });
+        }
+      });
+      put("1689492", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("124838");
+          setDescription("RASS = Target RASS -5: Unarousable (acceptable range -5 to -4)");
+          setOrderId("1689492");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.ACCEPTED);
+          setQuestions(new ArrayList<String>() {
+            {
+              add("Target RASS -5: Unarousable (acceptable range -5 to -4)");
+            }
+          });
+        }
+      });
+      put("1691295", new ExpectedOrder() {
+        {
+          setEncounter("2085202");
+          setProcedureId("143391");
+          setDescription("Bed Rest with HOB <= 30 Degrees Once");
+          setOrderId("1691295");
+          setStartDate(new Date(1439485200000l));
+          setStatus(ProcedureRequestStatusEnum.ACCEPTED);
+          setQuestions(new ArrayList<String>() {
+            {
+              add("Bathroom Privileges");
+              add("May stand to void");
+              add("Other (specify)");
+              add("Other (specify)");
+              add("May stand to void");
+            }
+          });
+        }
+      });
+    }
+  };
+
+  /**
+   * Encapsulates an expected order.
+   */
+  @Data
+  public static class ExpectedOrder {
+    private String encounter;
+    private String description;
+    private String orderId;
+    private String procedureId;
+    private ProcedureRequestStatusEnum status;
+    private Date startDate;
+    private Date discontinuedDate;
+    private List<String> questions;
+  }
+}

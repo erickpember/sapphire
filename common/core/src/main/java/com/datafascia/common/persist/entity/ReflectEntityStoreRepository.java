85661f2e4bcfa95d4289e144acf95aea6b838fa0
Chin Huang <chinhuang@datafascia.com>
2015-09-09 16:22:31 -0700

    Rename class to ReflectEntityStoreRepository

--- common/core/src/main/java/com/datafascia/common/persist/entity/ReflectEntityStoreRepository.java
+++ common/core/src/main/java/com/datafascia/common/persist/entity/ReflectEntityStoreRepository.java
@@ -1,6 +1,6 @@
 // Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
 // For license information, please contact http://datafascia.com/contact
-package com.datafascia.domain.persist;
+package com.datafascia.common.persist.entity;
 
 import com.datafascia.common.persist.entity.ReflectEntityStore;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
  * Implements common data access methods.
  */
 @Slf4j
-public abstract class EntityStoreRepository {
+public abstract class ReflectEntityStoreRepository {
 
   protected ReflectEntityStore entityStore;
 
@@ -19,7 +19,7 @@ public abstract class EntityStoreRepository {
    * @param entityStore
    *     entity store
    */
-  public EntityStoreRepository(ReflectEntityStore entityStore) {
+  public ReflectEntityStoreRepository(ReflectEntityStore entityStore) {
     this.entityStore = entityStore;
   }
 }

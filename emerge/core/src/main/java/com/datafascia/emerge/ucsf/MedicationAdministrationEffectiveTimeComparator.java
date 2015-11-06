4db59704f396413db2f96e0ced0445390b1d4bf4
Erick Pember <erick@datafascia.com>
2015-11-06 16:41:27 -0500

    Fixes potential casting issue in MedicationAdministration comparator.
    A MedicationAdministration's effective time field could be either
    DateTimeDt or Period.
    
    Made a change to handle both cases.
    
    Addresses a potential problem similar to SAPP-257

--- emerge/core/src/main/java/com/datafascia/emerge/ucsf/MedicationAdministrationEffectiveTimeComparator.java
+++ emerge/core/src/main/java/com/datafascia/emerge/ucsf/MedicationAdministrationEffectiveTimeComparator.java
@@ -2,21 +2,15 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.emerge.ucsf;
 
-import ca.uhn.fhir.model.api.IDatatype;
 import ca.uhn.fhir.model.dstu2.resource.MedicationAdministration;
-import ca.uhn.fhir.model.primitive.DateTimeDt;
+import com.datafascia.domain.fhir.Dates;
 import java.util.Comparator;
-import java.util.Date;
 
 /**
  * Compares the effective time property of medication administration resources.
  */
 public class MedicationAdministrationEffectiveTimeComparator implements
     Comparator<MedicationAdministration> {
-
-  private static Date toDate(IDatatype value) {
-    return ((DateTimeDt) value).getValue();
-  }
 
   @Override
   public int compare(MedicationAdministration left, MedicationAdministration right) {
@@ -29,6 +23,6 @@ public class MedicationAdministrationEffectiveTimeComparator implements
       return 1;
     }
 
-    return toDate(left.getEffectiveTime()).compareTo(toDate(right.getEffectiveTime()));
+    return Dates.toDate(left.getEffectiveTime()).compareTo(Dates.toDate(right.getEffectiveTime()));
   }
 }

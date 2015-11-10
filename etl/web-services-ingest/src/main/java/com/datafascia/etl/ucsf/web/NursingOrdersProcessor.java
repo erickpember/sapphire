01e42f64f691e4da9c90f1c9dbb68fb0b18fb6f1
Chin Huang <chinhuang@datafascia.com>
2015-11-10 14:32:25 -0800

    Extract nursing order transformation logic to new class
    So tests can invoke this new class instead of using NiFi's test harness.

--- etl/web-services-ingest/src/main/java/com/datafascia/etl/ucsf/web/NursingOrdersProcessor.java
+++ etl/web-services-ingest/src/main/java/com/datafascia/etl/ucsf/web/NursingOrdersProcessor.java
@@ -2,219 +2,88 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.etl.ucsf.web;
 
-import ca.uhn.fhir.model.dstu2.composite.AnnotationDt;
-import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
-import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
-import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
-import ca.uhn.fhir.model.dstu2.resource.Encounter;
-import ca.uhn.fhir.model.dstu2.resource.ProcedureRequest;
-import ca.uhn.fhir.model.dstu2.valueset.ProcedureRequestStatusEnum;
-import ca.uhn.fhir.model.primitive.DateTimeDt;
-import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
-import com.datafascia.api.client.ClientBuilder;
 import com.datafascia.common.nifi.DependencyInjectingProcessor;
-import com.datafascia.domain.fhir.CodingSystems;
-import com.datafascia.domain.fhir.IdentifierSystems;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.CharStreams;
 import com.google.inject.Inject;
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
+import java.util.concurrent.atomic.AtomicReference;
+import org.apache.nifi.annotation.behavior.EventDriven;
+import org.apache.nifi.annotation.behavior.SupportsBatching;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
-import org.apache.nifi.logging.ProcessorLog;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.Processor;
-import org.apache.nifi.processor.ProcessorInitializationContext;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
-import org.apache.nifi.processor.io.InputStreamCallback;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.kohsuke.MetaInfServices;
 
 /**
  * Processor for handling nursing orders at UCSF.
  */
+@CapabilityDescription("Transforms UCSF nursing order to FHIR procedure request.")
+@EventDriven
 @MetaInfServices(Processor.class)
-@Slf4j
+@SupportsBatching
 @Tags({"ingest", "datafascia", "ucsf", "json"})
 public class NursingOrdersProcessor extends DependencyInjectingProcessor {
-  private Set<Relationship> relationships;
-  private List<PropertyDescriptor> properties;
 
-  @Inject
-  private ClientBuilder clientBuilder = null;
-
-  public static final Relationship SUCCESS = new Relationship.Builder()
-      .name("SUCCESS")
-      .description("Success relationship")
-      .build();
   public static final Relationship FAILURE = new Relationship.Builder()
       .name("FAILURE")
-      .description("Failure relationship")
+      .description("Failed nursing orders")
       .build();
+  public static final Relationship SUCCESS = new Relationship.Builder()
+      .name("SUCCESS")
+      .description("Successfully processed nursing orders")
+      .build();
+
+  private static final Set<Relationship> RELATIONSHIPS = ImmutableSet.of(FAILURE, SUCCESS);
+
+  @Inject
+  private NursingOrdersTransformer nursingOrdersTransformer;
 
   @Override
   public Set<Relationship> getRelationships() {
-    return relationships;
+    return RELATIONSHIPS;
   }
 
   @Override
   public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-    return properties;
+    return Collections.emptyList();
   }
 
-  @Override
-  public void init(final ProcessorInitializationContext context) {
-    List<PropertyDescriptor> initProperties = new ArrayList<>();
-    this.properties = Collections.unmodifiableList(initProperties);
-
-    Set<Relationship> initRelationships = new HashSet<>();
-    initRelationships.add(SUCCESS);
-    initRelationships.add(FAILURE);
-    this.relationships = Collections.unmodifiableSet(initRelationships);
+  private String readString(InputStream input) throws IOException {
+    return CharStreams.toString(new InputStreamReader(input, StandardCharsets.UTF_8));
   }
 
   @Override
   public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
-    final ProcessorLog plog = this.getLogger();
-    final FlowFile flowfile = session.get();
-
-    session.read(flowfile, new InputStreamCallback() {
-
-      @Override
-      public void process(InputStream in) throws IOException {
-        try {
-          String jsonString = IOUtils.toString(in);
-          JSONObject jsonObject = (JSONObject) new JSONParser().parse(jsonString);
-
-          Object errorObj = jsonObject.get("Error");
-          if (errorObj != null && !errorObj.toString().equals("")) {
-            log.error("Nursing order web service error: " + errorObj.toString());
-          }
-
-          JSONArray patients = (JSONArray) jsonObject.get("Patient");
-          for (Object obj : patients) {
-            if (obj instanceof JSONObject) {
-              String encounterId = ((JSONObject) obj).get("CSN").toString();
-              JSONArray orders = (JSONArray) ((JSONObject) obj).get("Orders");
-              for (Object obj2 : orders) {
-                if (obj2 instanceof JSONObject) {
-                  JSONObject jsonNursing = (JSONObject) obj2;
-                  String status = jsonNursing.get("OrderStatus").toString();
-                  ProcedureRequest procedure = populateProcedureRequest(jsonNursing, encounterId);
-
-                  ProcedureRequest existingRequest = clientBuilder.getProcedureRequestClient()
-                      .getProcedureRequest(procedure.getIdentifierFirstRep().getValue(),
-                          encounterId);
-
-                  if (existingRequest == null) {
-                    clientBuilder.getProcedureRequestClient().saveProcedureRequest(procedure,
-                        encounterId);
-                  } else {
-                    clientBuilder.getProcedureRequestClient().updateProcedureRequest(procedure,
-                        encounterId);
-                  }
-                }
-              }
-            }
-          }
-        } catch (ParseException e) {
-          log.error("Error reading json.", e);
-          plog.error("Failed to read json string: " + e.getMessage());
-          throw new ProcessException(e);
-        }
-      }
-    });
-    session.transfer(flowfile, SUCCESS);
-  }
-
-  private ProcedureRequest populateProcedureRequest(JSONObject jsonNursing, String encounterId) {
-    String status = jsonNursing.get("OrderStatus").toString();
-    String orderId = jsonNursing.get("OrderID").toString();
-    String procID = jsonNursing.get("ProcID").toString();
-    String orderDesc = jsonNursing.get("OrderDesc").toString();
-    String startDate = jsonNursing.get("StartDate").toString();
-    String discontinuedDate = jsonNursing.get("DiscontinuedDate").toString();
-
-    ProcedureRequest procedureRequest = new ProcedureRequest();
-    procedureRequest.addIdentifier()
-        .setSystem(IdentifierSystems.INSTITUTION_PROCEDURE_REQUEST)
-        .setValue(orderId);
-
-    if (status.equalsIgnoreCase("2^SENT")) {
-      procedureRequest.setStatus(ProcedureRequestStatusEnum.IN_PROGRESS);
-    } else if (status.equalsIgnoreCase("6^HOLDING FOR REFERRAL")) {
-      procedureRequest.setStatus(ProcedureRequestStatusEnum.PROPOSED);
-    } else if (status.equalsIgnoreCase("7^DENIED APPROVAL")) {
-      procedureRequest.setStatus(ProcedureRequestStatusEnum.REJECTED);
-    } else if (status.equalsIgnoreCase("4^CANCELED")) {
-      procedureRequest.setStatus(ProcedureRequestStatusEnum.REJECTED);
-    } else if (status.equalsIgnoreCase("5^COMPLETED")) {
-      procedureRequest.setStatus(ProcedureRequestStatusEnum.COMPLETED);
+    FlowFile flowFile = session.get();
+    if (flowFile == null) {
+      return;
     }
 
-    PeriodDt period = new PeriodDt();
-    /* Null or otherwise 0 dates (they manifest differently from system to system) may throw
-     * exceptions which can be safely ignored. We only care about real data. */
     try {
-      Instant startInstant = UcsfWebGetProcessor.epicDateToInstant(startDate);
-      period.setStart(new DateTimeDt(Date.from(startInstant)));
-    } catch (DateTimeException e) {
+      AtomicReference<String> orderReference = new AtomicReference<>();
+      session.read(flowFile, input -> orderReference.set(readString(input)));
+      String order = orderReference.get();
+
+      nursingOrdersTransformer.accept(order);
+
+      session.transfer(flowFile, SUCCESS);
+    } catch (RuntimeException e) {
+      log.error("Cannot process {}", new Object[] { flowFile }, e);
+      flowFile = session.penalize(flowFile);
+      session.transfer(flowFile, FAILURE);
     }
-
-    // The string here is a null date, which we don't want to use.
-    if (!discontinuedDate.equals("/Date(-62135568000000-0800)/")) {
-      try {
-        Instant endInstant = UcsfWebGetProcessor.epicDateToInstant(discontinuedDate);
-        period.setEnd(new DateTimeDt(Date.from(endInstant)));
-      } catch (DateTimeException e) {
-      }
-    }
-
-    procedureRequest.setScheduled(period);
-
-    procedureRequest.setCode(
-        new CodeableConceptDt(CodingSystems.PROCEDURE_REQUEST, procID)
-        .setText(orderDesc));
-
-    Encounter encounter = null;
-    try {
-      encounter = clientBuilder.getEncounterClient().getEncounter(encounterId);
-    } catch (ResourceNotFoundException e) {
-    }
-
-    if (encounter != null) {
-      procedureRequest.setEncounter(new ResourceReferenceDt(encounter.getId()));
-    } else {
-      log.warn("Could not find encounter with CSN " + encounterId + ". HAPI FHIR doesn't allow "
-          + "dangling references, so the linkage between the encounter and nursing order "
-          + orderId + " is lost.");
-    }
-
-    List<AnnotationDt> notes = new ArrayList<>();
-    Object questions = jsonNursing.get("Questions");
-    if (questions != null) {
-      for (Object obj : ((JSONArray) questions)) {
-        String answer = ((JSONObject) obj).get("Answer").toString();
-        notes.add(new AnnotationDt().setText(answer));
-      }
-    }
-    procedureRequest.setNotes(notes);
-
-    return procedureRequest;
   }
 }
---
+++ etl/web-services-ingest/src/main/java/com/datafascia/etl/ucsf/web/NursingOrdersTransformer.java
@@ -0,0 +1,154 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.etl.ucsf.web;
+
+import ca.uhn.fhir.model.dstu2.composite.AnnotationDt;
+import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
+import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
+import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import ca.uhn.fhir.model.dstu2.resource.ProcedureRequest;
+import ca.uhn.fhir.model.dstu2.valueset.ProcedureRequestStatusEnum;
+import ca.uhn.fhir.model.primitive.DateTimeDt;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import com.datafascia.api.client.ClientBuilder;
+import com.datafascia.domain.fhir.CodingSystems;
+import com.datafascia.domain.fhir.IdentifierSystems;
+import com.google.inject.Inject;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * Transforms UCSF nursing orders to procedure requests.
+ */
+@Slf4j
+public class NursingOrdersTransformer {
+
+  @Inject
+  private ClientBuilder apiClient;
+
+  /**
+   * Transforms UCSF nursing order to procedure request.
+   *
+   * @param jsonString
+   *     nursing order in JSON format
+   */
+  public void accept(String jsonString) {
+    JSONObject jsonObject;
+    try {
+      jsonObject = (JSONObject) new JSONParser().parse(jsonString);
+    } catch (ParseException e) {
+      throw new IllegalStateException("Cannot parse JSON " + jsonString, e);
+    }
+
+    Object errorObj = jsonObject.get("Error");
+    if (errorObj != null && !errorObj.toString().equals("")) {
+      log.error("Nursing order web service error: " + errorObj.toString());
+    }
+
+    JSONArray patients = (JSONArray) jsonObject.get("Patient");
+    for (Object patient : patients) {
+      if (patient instanceof JSONObject) {
+        String encounterId = ((JSONObject) patient).get("CSN").toString();
+        JSONArray orders = (JSONArray) ((JSONObject) patient).get("Orders");
+        for (Object order : orders) {
+          if (order instanceof JSONObject) {
+            ProcedureRequest procedure = populateProcedureRequest((JSONObject) order, encounterId);
+
+            ProcedureRequest existingRequest = apiClient.getProcedureRequestClient()
+                .getProcedureRequest(
+                    procedure.getIdentifierFirstRep().getValue(),
+                    encounterId);
+
+            if (existingRequest == null) {
+              apiClient.getProcedureRequestClient().saveProcedureRequest(
+                  procedure, encounterId);
+            } else {
+              apiClient.getProcedureRequestClient().updateProcedureRequest(
+                  procedure, encounterId);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private ProcedureRequest populateProcedureRequest(JSONObject order, String encounterId) {
+    String status = order.get("OrderStatus").toString();
+    String orderId = order.get("OrderID").toString();
+    String procID = order.get("ProcID").toString();
+    String orderDesc = order.get("OrderDesc").toString();
+    String startDate = order.get("StartDate").toString();
+    String discontinuedDate = order.get("DiscontinuedDate").toString();
+
+    ProcedureRequest procedureRequest = new ProcedureRequest();
+    procedureRequest.addIdentifier()
+        .setSystem(IdentifierSystems.INSTITUTION_PROCEDURE_REQUEST)
+        .setValue(orderId);
+
+    if (status.equalsIgnoreCase("2^SENT")) {
+      procedureRequest.setStatus(ProcedureRequestStatusEnum.IN_PROGRESS);
+    } else if (status.equalsIgnoreCase("6^HOLDING FOR REFERRAL")) {
+      procedureRequest.setStatus(ProcedureRequestStatusEnum.PROPOSED);
+    } else if (status.equalsIgnoreCase("7^DENIED APPROVAL")) {
+      procedureRequest.setStatus(ProcedureRequestStatusEnum.REJECTED);
+    } else if (status.equalsIgnoreCase("4^CANCELED")) {
+      procedureRequest.setStatus(ProcedureRequestStatusEnum.REJECTED);
+    } else if (status.equalsIgnoreCase("5^COMPLETED")) {
+      procedureRequest.setStatus(ProcedureRequestStatusEnum.COMPLETED);
+    }
+
+    PeriodDt period = new PeriodDt();
+    /* Null or otherwise 0 dates (they manifest differently from system to system) may throw
+     * exceptions which can be safely ignored. We only care about real data. */
+    try {
+      Instant startInstant = UcsfWebGetProcessor.epicDateToInstant(startDate);
+      period.setStart(new DateTimeDt(Date.from(startInstant)));
+    } catch (DateTimeException e) {
+    }
+
+    // The string here is a null date, which we don't want to use.
+    if (!discontinuedDate.equals("/Date(-62135568000000-0800)/")) {
+      try {
+        Instant endInstant = UcsfWebGetProcessor.epicDateToInstant(discontinuedDate);
+        period.setEnd(new DateTimeDt(Date.from(endInstant)));
+      } catch (DateTimeException e) {
+      }
+    }
+
+    procedureRequest.setScheduled(period);
+
+    procedureRequest.setCode(
+        new CodeableConceptDt(CodingSystems.PROCEDURE_REQUEST, procID)
+            .setText(orderDesc));
+
+    try {
+      Encounter encounter = apiClient.getEncounterClient().getEncounter(encounterId);
+      procedureRequest.setEncounter(new ResourceReferenceDt(encounter));
+    } catch (ResourceNotFoundException e) {
+      log.warn("Could not find encounter with CSN " + encounterId + ". HAPI FHIR doesn't allow "
+          + "dangling references, so the linkage between the encounter and nursing order " + orderId
+          + " is lost.", e);
+    }
+
+    List<AnnotationDt> notes = new ArrayList<>();
+    Object questions = order.get("Questions");
+    if (questions != null) {
+      for (Object obj : ((JSONArray) questions)) {
+        String answer = ((JSONObject) obj).get("Answer").toString();
+        notes.add(new AnnotationDt().setText(answer));
+      }
+    }
+    procedureRequest.setNotes(notes);
+
+    return procedureRequest;
+  }
+}

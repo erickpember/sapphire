35282024c17ac93ed6beed71416b76f265016f17
Erick Pember <erick@datafascia.com>
2015-11-10 20:32:38 -0500

    Fixes MedsSet access in PharmacologicVpeProphylaxisAdministered.
    A problem similar to the one mentioned in SAPP-266 has been
    found in this class. MedsSet wasn't getting retrieved in a way
    that works or doesn't throw exceptions.

--- emerge/core/src/main/java/com/datafascia/emerge/harms/vte/PharmacologicVteProphylaxisAdministered.java
+++ emerge/core/src/main/java/com/datafascia/emerge/harms/vte/PharmacologicVteProphylaxisAdministered.java
@@ -2,13 +2,13 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.emerge.harms.vte;
 
-import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
-import ca.uhn.fhir.model.dstu2.resource.Medication;
+import ca.uhn.fhir.model.dstu2.composite.IdentifierDt;
 import ca.uhn.fhir.model.dstu2.resource.MedicationAdministration;
-import ca.uhn.fhir.model.dstu2.resource.MedicationOrder;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
 import com.datafascia.api.client.ClientBuilder;
+import com.datafascia.domain.fhir.CodingSystems;
 import com.datafascia.emerge.harms.HarmsLookups;
+import com.datafascia.emerge.ucsf.MedicationAdministrationUtils;
 import java.util.List;
 import javax.inject.Inject;
 
@@ -35,23 +35,18 @@ public class PharmacologicVteProphylaxisAdministered {
 
     // Check if any recent VTE prophylactic administrations have been made.
     for (MedicationAdministration administration : administrations) {
-      ResourceReferenceDt prescriptionReference = administration.getPrescription();
-      MedicationOrder medicationOrder = apiClient.getMedicationOrderClient()
-          .read(prescriptionReference.getReference().getIdPart(), encounterId);
+      for (IdentifierDt ident : MedicationAdministrationUtils.findIdentifiers(administration,
+          CodingSystems.UCSF_MEDICATION_GROUP_NAME)) {
+        String medsSet = ident.getValue();
 
-      ResourceReferenceDt medicationReference =
-          (ResourceReferenceDt) medicationOrder.getMedication();
-      Medication medication = apiClient.getMedicationClient()
-          .getMedication(medicationReference.getReference().getIdPart());
-
-      String medicationName = medication.getCode().getText();
-      for (PharmacologicVtePpxTypeEnum vtePpx : PharmacologicVtePpxTypeEnum.values()) {
-        DateTimeDt timeTaken = (DateTimeDt) administration.getEffectiveTime();
-        Long period = HarmsLookups.efficacyList.get(medicationName);
-        if (vtePpx.toString().equalsIgnoreCase(medicationName.replace(" ", "_"))
-            && HarmsLookups.withinDrugPeriod(timeTaken.getValue(), period)) {
-          administered = true;
-          break;
+        for (PharmacologicVtePpxTypeEnum vtePpx : PharmacologicVtePpxTypeEnum.values()) {
+          DateTimeDt timeTaken = (DateTimeDt) administration.getEffectiveTime();
+          Long period = HarmsLookups.efficacyList.get(medsSet);
+          if (vtePpx.toString().equalsIgnoreCase(medsSet.replace(" ", "_"))
+              && HarmsLookups.withinDrugPeriod(timeTaken.getValue(), period)) {
+            administered = true;
+            break;
+          }
         }
       }
     }

7f1158f99983023387fbcc78eab654ff422df44f
jimsmyth <jimsmyth@datafascia.com>
2015-11-11 07:30:06 -0500

    Implements Code<String> for PharmacologicVtePpxTypeEnum

--- emerge/core/src/main/java/com/datafascia/emerge/harms/vte/PharmacologicVtePpxTypeEnum.java
+++ emerge/core/src/main/java/com/datafascia/emerge/harms/vte/PharmacologicVtePpxTypeEnum.java
@@ -2,10 +2,39 @@
 // For license information, please contact http://datafascia.com/contact
 package com.datafascia.emerge.harms.vte;
 
+import com.datafascia.common.persist.Code;
+import com.datafascia.common.persist.CodeToEnumMapper;
+import java.util.Optional;
+
 /**
  * Enumeration of pharmacologic VTE prophylaxis types.
  */
-public enum PharmacologicVtePpxTypeEnum {
-  INTERMITTENT_ENOXAPARIN,
-  INTERMITTENT_HEPARIN_SC
+public enum PharmacologicVtePpxTypeEnum implements Code<String> {
+  INTERMITTENT_ENOXAPARIN("Intermittent Enoxaparin"),
+  INTERMITTENT_HEPARIN_SC("Intermittent Heparin SC");
+
+  private final String code;
+
+  private static final CodeToEnumMapper<String, PharmacologicVtePpxTypeEnum> CODE_TO_ENUM_MAPPER =
+      new CodeToEnumMapper<>(PharmacologicVtePpxTypeEnum.class);
+
+  PharmacologicVtePpxTypeEnum(String code) {
+    this.code = code;
+  }
+
+  @Override
+  public String getCode() {
+    return code;
+  }
+
+  /**
+   * Converts code to enum constant.
+   *
+   * @param code
+   *     input code
+   * @return optional enum constant, empty if code is unknown
+   */
+  public static Optional<PharmacologicVtePpxTypeEnum> of(String code) {
+    return CODE_TO_ENUM_MAPPER.of(code);
+  }
 }

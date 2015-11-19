f20768497c9266377fe5750db6eb88ab0ac5b27b
Erick Pember <erick@datafascia.com>
2015-11-18 19:04:34 -0500

    Reworks SedativeOrder logic to avoid arrays.
    Array index out of bounds exceptions were happening.
    Reworked SedativeOrder to not use them, wrote a unit
    test for the new logic.
    
    Addresses SAPP-366

--- emerge/core/src/main/java/com/datafascia/emerge/harms/pain/SedativeOrder.java
+++ emerge/core/src/main/java/com/datafascia/emerge/harms/pain/SedativeOrder.java
@@ -8,6 +8,7 @@ import com.datafascia.api.client.ClientBuilder;
 import com.datafascia.domain.fhir.CodingSystems;
 import com.datafascia.emerge.ucsf.MedicationOrderDateWrittenComparator;
 import com.datafascia.emerge.ucsf.MedicationOrderUtils;
+import com.datafascia.emerge.ucsf.codes.MedsSetEnum;
 import com.datafascia.emerge.ucsf.codes.painAndDelerium.SedativeOrderDosageRouteEnum;
 import com.datafascia.emerge.ucsf.codes.painAndDelerium.SedativeOrderDrugEnum;
 import com.google.common.collect.ImmutableSet;
@@ -35,6 +36,18 @@ public class SedativeOrder {
       SedativeOrderDrugEnum.DIAZEPAM.getCode(),
       SedativeOrderDrugEnum.CHLORADIAZEPOXIDE.getCode(),
       SedativeOrderDrugEnum.ALPRAZOLOM.getCode());
+
+  private static final Set<String> SEDATIVE_MEDS_SETS = ImmutableSet.of(
+      MedsSetEnum.CONTINUOUS_INFUSION_LORAZEPAM_IV.getCode(),
+      MedsSetEnum.CONTINUOUS_INFUSION_MIDAZOLAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_ALPRAZALOM_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_CHLORADIAZEPOXIDE_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_CLONAZEPAM_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_DIAZEPAM_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_DIAZEPAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_LORAZEPAM_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_LORAZEPAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_MIDAZOLAM_IV.getCode());
 
   private static final Set<String> DOSAGE_ROUTES = ImmutableSet.of(
       SedativeOrderDosageRouteEnum.INTERMITTENT_IV.getCode(),
@@ -68,11 +81,6 @@ public class SedativeOrder {
    */
   public Optional<OrderResult> getValue(String encounterId, String medication) {
     Date now = Date.from(Instant.now(clock));
-    OrderResult result = OrderResult.builder()
-        .dosageRoute(null)
-        .drug(null)
-        .status(null)
-        .build();
 
     List<MedicationOrder> medOrders = MedicationOrderUtils
         .findActiveOrDraftOrderForMedicationBeforeTime(
@@ -82,23 +90,51 @@ public class SedativeOrder {
             now);
     medOrders.sort(new MedicationOrderDateWrittenComparator().reversed());
 
+    return processOrders(medOrders);
+  }
+
+  /**
+   * Handles the client-independent logic of medication orders in, drug/dosage results out.
+   * @param medOrders
+   *     A list of medication orders whose meds set identifiers we will scan.
+   * @return
+   *     Drug name, route and order status, empty if not found.
+   */
+  public Optional<OrderResult> processOrders(List<MedicationOrder> medOrders) {
     for (MedicationOrder order : medOrders) {
+      OrderResult result = OrderResult.builder()
+          .dosageRoute(null)
+          .drug(null)
+          .status(null)
+          .build();
+
       List<IdentifierDt> idents = MedicationOrderUtils.findIdentifiers(order,
           CodingSystems.UCSF_MEDICATION_GROUP_NAME);
       for (IdentifierDt ident : idents) {
-        String[] identifierParts = ident.getValue().split(" ");
-        if (ident.getValue().contains("Continuous")) {
-          result.setDosageRoute(identifierParts[1] + " " + identifierParts[2] + " "
-              + identifierParts[4]);
-          result.setDosageRoute(identifierParts[3]);
-          result.setStatus(order.getStatus());
+        String medsSet = ident.getValue();
 
-          return verifyResult(result);
-        } else if (ident.getValue().contains("Intermittent")) {
-          result.setDosageRoute(identifierParts[1] + " " + identifierParts[3]);
-          result.setDosageRoute(identifierParts[2]);
-          result.setStatus(order.getStatus());
-          return verifyResult(result);
+        if (SEDATIVE_MEDS_SETS.contains(medsSet)) {
+          for (String sedativeName : SEDATIVE_NAMES) {
+            if (medsSet.contains(sedativeName)) {
+              result.setDrug(sedativeName);
+
+              if (medsSet.contains("Continuous")) {
+                if (medsSet.contains(" IV")) {
+                  result.setDosageRoute(SedativeOrderDosageRouteEnum.CONTINUOUS_INFUSION_IV
+                      .getCode());
+                }
+              } else if (medsSet.contains("Intermittent")) {
+                if (medsSet.contains(" IV")) {
+                  result.setDosageRoute(SedativeOrderDosageRouteEnum.INTERMITTENT_IV.getCode());
+                }
+                if (medsSet.contains("Enteral")) {
+                  result.setDosageRoute(
+                      SedativeOrderDosageRouteEnum.INTERMITTENT_ENTERAL.getCode());
+                }
+              }
+              return verifyResult(result);
+            }
+          }
         }
       }
     }
---
+++ emerge/core/src/test/java/com/datafascia/emerge/harms/pain/SedativeOrderTest.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2015-2016 dataFascia Corporation - All Rights Reserved
+// For license information, please contact http://datafascia.com/contact
+package com.datafascia.emerge.harms.pain;
+
+import ca.uhn.fhir.model.dstu2.resource.MedicationOrder;
+import com.datafascia.domain.fhir.CodingSystems;
+import com.datafascia.emerge.ucsf.codes.MedsSetEnum;
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Test of SedativeOrder
+ */
+public class SedativeOrderTest {
+
+  Set<String> BENZODIAZEPINE_NAMES = ImmutableSet.of(
+      MedsSetEnum.INTERMITTENT_LORAZEPAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_LORAZEPAM_ENTERAL.getCode(),
+      MedsSetEnum.CONTINUOUS_INFUSION_LORAZEPAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_MIDAZOLAM_IV.getCode(),
+      MedsSetEnum.CONTINUOUS_INFUSION_MIDAZOLAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_CLONAZEPAM_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_DIAZEPAM_IV.getCode(),
+      MedsSetEnum.INTERMITTENT_DIAZEPAM_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_CHLORADIAZEPOXIDE_ENTERAL.getCode(),
+      MedsSetEnum.INTERMITTENT_ALPRAZALOM_ENTERAL.getCode());
+
+  /**
+   * Test of processOrders method, of class SedativeOrder.
+   */
+  @Test
+  public void testProcessOrders() {
+
+    SedativeOrder sedativeOrderImpl = new SedativeOrder();
+
+    for (String benzo : BENZODIAZEPINE_NAMES) {
+      List<MedicationOrder> orders = new ArrayList<>();
+      orders.add(createMedicationOrder(benzo));
+
+      assertTrue(sedativeOrderImpl.processOrders(orders).isPresent());
+    }
+  }
+
+  private MedicationOrder createMedicationOrder(
+      String identifier) {
+
+    MedicationOrder prescription = new MedicationOrder();
+    prescription.addIdentifier()
+        .setSystem(CodingSystems.UCSF_MEDICATION_GROUP_NAME)
+        .setValue(identifier);
+    return prescription;
+  }
+}
